b4e5bce4a28ac3b9489d81cb6240beef12c6e766
tephaMR22 <stephania.martinez.rodriguez@correounivalle.edu.co>
2022-05-13 21:15:41 -0500

    Update

--- registroUsuario.py
+++ registroUsuario.py
@@ -23,97 +23,97 @@ class registroUsuario:
         Label(self.rootRegistroUsuario, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
 
 
-        # ****** Frame inicio Productos Side Der ****** #
+        # ****** Frame Crear Usuario ****** #
 
-        frameIzquierdoCrearUsu = Frame(self.rootRegistroUsuario,bg="#18344A")
-        frameIzquierdoCrearUsu.place(x=160, y=0, width=650, height=600)
+        frameCrearUsuario = Frame(self.rootRegistroUsuario,bg="#18344A")
+        frameCrearUsuario.place(x=160, y=0, width=650, height=600)
 
-        Label(frameIzquierdoCrearUsu, text="REGISTRAR USUARIO", font=("comic sans MS", 25, "bold"), bg="#18344A",
+        Label(frameCrearUsuario, text="REGISTRAR USUARIO", font=("comic sans MS", 25, "bold"), bg="#18344A",
               fg="white").place(x=50, y=20)
 
-        # ------Row 1---
+        # ****** Label ID Crear Usuario ****** #
 
-        Label(frameIzquierdoCrearUsu, text="Identificador: ", font=("comic sans MS", 16, "bold"), bg="#18344A", fg="white").place(x=50, y=70)
-        self.ident = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
+        Label(frameCrearUsuario, text="Identificador: ", font=("comic sans MS", 16, "bold"), bg="#18344A", fg="white").place(x=50, y=70)
+        self.ident = Entry(frameCrearUsuario, font=("comic sans MS", 16))
         self.ident.place(x=50, y=110)
 
-        Label(frameIzquierdoCrearUsu, text="Nombre: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
+        # ****** Label Nombre Crear Usuario ****** #
+
+        Label(frameCrearUsuario, text="Nombre: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
               fg="white").place(x=350, y=70)
-        self.nombre = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
+        self.nombre = Entry(frameCrearUsuario, font=("comic sans MS", 16))
         self.nombre.place(x=350, y=110)
 
-        # ------Row 2---
+        # ****** Label Apellido Crear Usuario ****** #
 
-        Label(frameIzquierdoCrearUsu, text="Apellido: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
+        Label(frameCrearUsuario, text="Apellido: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
               fg="white").place(x=50, y=150)
-        self.apell = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
+        self.apell = Entry(frameCrearUsuario, font=("comic sans MS", 16))
         self.apell.place(x=50, y=190)
 
-        Label(frameIzquierdoCrearUsu, text="Telefono: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
+        # ****** Label Telefono Crear Usuario ****** #
+
+        Label(frameCrearUsuario, text="Telefono: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
               fg="white").place(x=350, y=150)
-        self.tel = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
+        self.tel = Entry(frameCrearUsuario, font=("comic sans MS", 16))
         self.tel.place(x=350, y=190)
 
-        # ------Row 3---
+        # ****** Label Direccion Crear Usuario ****** #
 
-        Label(frameIzquierdoCrearUsu, text="Direccion ", font=("comic sans MS", 16, "bold"), bg="#18344A",fg="white").place(x=50, y=230)
-        self.dir = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
+        Label(frameCrearUsuario, text="Direccion ", font=("comic sans MS", 16, "bold"), bg="#18344A",fg="white").place(x=50, y=230)
+        self.dir = Entry(frameCrearUsuario, font=("comic sans MS", 16))
         self.dir.place(x=50, y=270)
 
-        Label(frameIzquierdoCrearUsu, text="Cargo ", font=("comic sans MS", 16, "bold"), bg="#18344A", fg="white").place(x=350, y=230)
-        self.rolPass = ttk.Combobox(frameIzquierdoCrearUsu, font=("comic sans MS",13), state="readonly", justify=CENTER)
+        # ****** Label Cargo Crear Usuario ****** #
+
+        Label(frameCrearUsuario, text="Cargo ", font=("comic sans MS", 16, "bold"), bg="#18344A", fg="white").place(x=350, y=230)
+        self.rolPass = ttk.Combobox(frameCrearUsuario, font=("comic sans MS",13), state="readonly", justify=CENTER)
         self.rolPass["values"] = ["vendedor","bodega","contador", "administrador"]
         self.rolPass.place(x=350,y=270)
 
         self.carg = self.rolPass
-        #self.carg = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
-        #self.carg.place(x=350, y=270)
 
-        # ------Row 4---
+        # ****** Label Sueldo Crear Usuario ****** #
 
-        Label(frameIzquierdoCrearUsu, text="Sueldo ", font=("comic sans MS", 16, "bold"), bg="#18344A",fg="white").place(x=50, y=310)
-        self.sueld = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
+        Label(frameCrearUsuario, text="Sueldo ", font=("comic sans MS", 16, "bold"), bg="#18344A",fg="white").place(x=50, y=310)
+        self.sueld = Entry(frameCrearUsuario, font=("comic sans MS", 16))
         self.sueld.place(x=50, y=350)
 
         self.contraseña = self.ident
 
-        # ------Row 5---
+        # ****** Boton Guardar Crear Usuario ****** #
 
-
-
-        # ------Row 6---
-        BotonGuardar = Button(frameIzquierdoCrearUsu, text="Registrar", command=self.registrar, font=("comic sans MS", 15), bd=0,
-                              cursor="hand2")
+        BotonGuardar = Button(frameCrearUsuario, text="Registrar", command=self.registrar, font=("comic sans MS", 15), bd=0,cursor="hand2")
         BotonGuardar.place(x=50, y=500, width=200)
-        BotonLogin = Button(frameIzquierdoCrearUsu, text="Volver", command=self.volver, font=("comic sans MS", 15),
-                            bd=0, cursor="hand2")
+
+        # ****** Label Volver Crear Usuario ****** #
+
+        BotonLogin = Button(frameCrearUsuario, text="Volver", command=self.volver, font=("comic sans MS", 15),bd=0, cursor="hand2")
         BotonLogin.place(x=350, y=500, width=200)
 
     def validacion(self):
-        return (len(self.ident.get()) == 0 or len(self.carg.get()) == 0) #<--
+        return (len(self.ident.get()) == 0 or len(self.carg.get()) == 0)
 
     def registrar(self):
-
         if self.validacion():
             messagebox.showinfo("error!", "Los datos son obligatorios")
         else:
             self.gestionUsuario = gestionUsuario()
-            self.gestionUsuario.registrar_usuario(self.ident.get(), self.nombre.get(), self.sueld.get(),self.tel.get(),
-                                                  self.dir.get(), self.contraseña.get(),self.carg.get(),
-                                                  self.apell.get())
+            self.gestionUsuario.registrar_usuario(self.ident.get(), self.nombre.get(), self.sueld.get(),self.tel.get(),self.dir.get(), self.contraseña.get(),self.carg.get(),self.apell.get())
             self.volver()
 
-    # Para ir al Login
-    def volver(self):
-        self.rootRegistroUsuario.destroy()
-        import GUIGestionEmpleado as ges
-        ges.iniciar()
+        # ****** Para volver al login desde Crear Usuario ****** #
+        def volver(self):
+            self.rootRegistroUsuario.destroy()
+            import GUIGestionEmpleado as ges
+            ges.iniciar()
 
+            # ****** Metodo para iniciar la interfaz desde otra ****** #
 
-def iniciar():
-    rootRegistroUsuario = Tk()
-    obj = registroUsuario(rootRegistroUsuario)
-    rootRegistroUsuario.mainloop()
+        def iniciar():
+            rootRegistroUsuario = Tk()
+            obj = registroUsuario(rootRegistroUsuario)
+            rootRegistroUsuario.mainloop()
 
 rootRegistroUsuario = Tk()
 obj = registroUsuario(rootRegistroUsuario)
