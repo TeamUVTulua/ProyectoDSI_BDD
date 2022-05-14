a98c8b8a06df4a9897b2ee46c50b2c87153290cd
tephaMR22 <stephania.martinez.rodriguez@correounivalle.edu.co>
2022-05-13 21:02:29 -0500

    Update registroUsuario.py

--- registroUsuario.py
+++ registroUsuario.py
@@ -11,70 +11,68 @@ from gestionUsuario import *
 
 class registroUsuario:
 
-    def __init__(self, rootRegistroUsuario)
+    def __init__(self, rootRegistroUsuario):
         self.rootRegistroUsuario = rootRegistroUsuario
         self.rootRegistroUsuario.title("Registro Usuario")
         self.rootRegistroUsuario.geometry("1360x768+560+312")
         self.rootRegistroUsuario.resizable(1, 1)
 
-        # --------imagen background------
+        # ******logo de Fondo****** #
 
         self.bg = ImageTk.PhotoImage(file="Imagenes\FondoInterfaz2.png")
         Label(self.rootRegistroUsuario, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
 
 
-        # -------registrerFrame--------
+        # ****** Frame inicio Productos Side Der ****** #
 
-        miFrame1 = Frame(self.rootRegistroUsuario bg="saddlebrown")
-        miFrame1.place(x=160, y=0, width=650, height=600)
+        frameIzquierdoCrearUsu = Frame(self.rootRegistroUsuario,bg="#18344A")
+        frameIzquierdoCrearUsu.place(x=160, y=0, width=650, height=600)
 
-        Label(miFrame1, text="REGISTRAR USUARIO", font=("times new roman", 25, "bold"), bg="saddlebrown",
+        Label(frameIzquierdoCrearUsu, text="REGISTRAR USUARIO", font=("comic sans MS", 25, "bold"), bg="#18344A",
               fg="white").place(x=50, y=20)
 
         # ------Row 1---
 
-        Label(miFrame1, text="Identificador: ", font=("times new roman", 16, "bold"), bg="saddlebrown", fg="gainsboro").place(x=50, y=70)
-        self.ident = Entry(miFrame1, font=("times new roman", 16))
+        Label(frameIzquierdoCrearUsu, text="Identificador: ", font=("comic sans MS", 16, "bold"), bg="#18344A", fg="white").place(x=50, y=70)
+        self.ident = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
         self.ident.place(x=50, y=110)
 
-        Label(miFrame1, text="Nombre: ", font=("times new roman", 16, "bold"), bg="saddlebrown",
-              fg="gainsboro").place(x=350, y=70)
-        self.nombre = Entry(miFrame1, font=("times new roman", 16))
+        Label(frameIzquierdoCrearUsu, text="Nombre: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
+              fg="white").place(x=350, y=70)
+        self.nombre = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
         self.nombre.place(x=350, y=110)
 
         # ------Row 2---
 
-        Label(miFrame1, text="Apellido: ", font=("times new roman", 16, "bold"), bg="saddlebrown",
-              fg="gainsboro").place(x=50, y=150)
-        self.apell = Entry(miFrame1, font=("times new roman", 16))
+        Label(frameIzquierdoCrearUsu, text="Apellido: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
+              fg="white").place(x=50, y=150)
+        self.apell = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
         self.apell.place(x=50, y=190)
 
-        Label(miFrame1, text="Telefono: ", font=("times new roman", 16, "bold"), bg="saddlebrown",
-              fg="gainsboro").place(x=350, y=150)
-        self.tel = Entry(miFrame1, font=("times new roman", 16))
+        Label(frameIzquierdoCrearUsu, text="Telefono: ", font=("comic sans MS", 16, "bold"), bg="#18344A",
+              fg="white").place(x=350, y=150)
+        self.tel = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
         self.tel.place(x=350, y=190)
 
         # ------Row 3---
 
-        Label(miFrame1, text="Direccion ", font=("times new roman", 16, "bold"), bg="saddlebrown",
-              fg="gainsboro").place(x=50, y=230)
-        self.dir = Entry(miFrame1, font=("times new roman", 16))
+        Label(frameIzquierdoCrearUsu, text="Direccion ", font=("comic sans MS", 16, "bold"), bg="#18344A",fg="white").place(x=50, y=230)
+        self.dir = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
         self.dir.place(x=50, y=270)
 
-        Label(miFrame1, text="Cargo ", font=("times new roman", 16, "bold"), bg="saddlebrown", fg="gainsboro").place(x=350, y=230)
-        self.rolPass = ttk.Combobox(miFrame1, font=("times new roman",13), state="readonly", justify=CENTER)
+        Label(frameIzquierdoCrearUsu, text="Cargo ", font=("comic sans MS", 16, "bold"), bg="#18344A", fg="white").place(x=350, y=230)
+        self.rolPass = ttk.Combobox(frameIzquierdoCrearUsu, font=("comic sans MS",13), state="readonly", justify=CENTER)
         self.rolPass["values"] = ["vendedor","bodega","contador", "administrador"]
         self.rolPass.place(x=350,y=270)
 
         self.carg = self.rolPass
-        #self.carg = Entry(miFrame1, font=("times new roman", 16))
+        #self.carg = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
         #self.carg.place(x=350, y=270)
 
         # ------Row 4---
 
-        Label(miFrame1, text="Sueldo ", font=("times new roman", 16, "bold"), bg="saddlebrown",
-              fg="gainsboro").place(x=50, y=310)
-        self.sueld = Entry(miFrame1, font=("times new roman", 16))
+        Label(frameIzquierdoCrearUsu, text="Sueldo ", font=("comic sans MS", 16, "bold"), bg="#18344A",fg="white").place(x=50, y=310)
+        self.sueld = Entry(frameIzquierdoCrearUsu, font=("comic sans MS", 16))
         self.sueld.place(x=50, y=350)
 
         self.contraseña = self.ident
@@ -84,10 +82,10 @@ class registroUsuario:
 
 
         # ------Row 6---
-        BotonGuardar = Button(miFrame1, text="Registrar", command=self.registrar, font=("times new roman", 15), bd=0,
+        BotonGuardar = Button(frameIzquierdoCrearUsu, text="Registrar", command=self.registrar, font=("comic sans MS", 15), bd=0,
                               cursor="hand2")
         BotonGuardar.place(x=50, y=500, width=200)
-        BotonLogin = Button(miFrame1, text="Volver", command=self.volver, font=("times new roman", 15),
+        BotonLogin = Button(frameIzquierdoCrearUsu, text="Volver", command=self.volver, font=("comic sans MS", 15),
                             bd=0, cursor="hand2")
         BotonLogin.place(x=350, y=500, width=200)
 
@@ -113,6 +111,10 @@ class registroUsuario:
 
 
 def iniciar():
-    rootRegistroUsua = Tk()
+    rootRegistroUsuario = Tk()
     obj = registroUsuario(rootRegistroUsuario)
-    rootRegistroUsua.mainloop()
+    rootRegistroUsuario.mainloop()
+
+rootRegistroUsuario = Tk()
+obj = registroUsuario(rootRegistroUsuario)
+rootRegistroUsuario.mainloop()
