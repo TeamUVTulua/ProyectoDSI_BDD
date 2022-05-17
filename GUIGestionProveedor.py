480733f825cd41fb3ed8df7d143a0743fed2b406
SirPeace12 <miguelangelpazvelasco@gmail.com>
2022-05-16 22:07:10 -0500

    Update

--- GUIGestionProveedor.py
+++ GUIGestionProveedor.py
@@ -173,7 +173,7 @@ class GUIProveedor:
                 showinfo('Modificación de información', 'No se realizó ningun cambio')
             else:
                 showinfo('Modificación de información', 'Tus telefono quedaron: {}'.format(aux4))
-                gestionUsuarios.modificar_direccion( self.nit)
+                gestionUsuarios.modificar_direccion( self.nit) 
             print(aux4)
 
     def CargarInfoUsuarioEnLabels2(self):
